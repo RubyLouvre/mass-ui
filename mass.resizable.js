5639492e44a144cd7fc6a5fde6cec5bb663c3edf
RubyLouvre <cheng19840218@gmail.com>
2013-01-27 17:23:55 +0800

    更新resizable的示例

--- mass.resizable.js
+++ mass.resizable.js
@@ -8,20 +8,19 @@ define("resizable", ["mass.draggable"], function($) {
         maxWidth: 10000,
         minHeight: 10,
         minWidth: 10,
-        noCursor: true,
+        cursor: false,
         edge: 5,
         resizestart: $.noop,
         resize: $.noop,
         resizeend: $.noop
     }
-    var facade = $.fn.draggable;
-  
     /**
- * 用于修正拖动元素靠边边缘的区域的鼠标样式
- * @param {Event} e
- * @param {Mass} target
- * @param {Object} data 经过处理的配置对象
- */
+     * 用于修正拖动元素靠边边缘的区域的鼠标样式
+     * @param {Event} e
+     * @param {Mass} target
+     * @param {Object} data 经过处理的配置对象
+     */
+
     function getDirection(e, target, data) {
         var dir = '';
         var offset = target.offset();
@@ -46,7 +45,7 @@ define("resizable", ["mass.draggable"], function($) {
         return '';
     }
 
-    function getCssValue(el, css) {//对样式值进行处理,强制转数值
+    function getCssValue(el, css) { //对样式值进行处理,强制转数值
         var val = parseInt(el.css(css), 10);
         if(isNaN(val)) {
             return 0;
@@ -55,19 +54,19 @@ define("resizable", ["mass.draggable"], function($) {
         }
     }
 
-    function refresh(e, target, data) {//刷新缩放元素
+    function refresh(event, target, data) { //刷新缩放元素
         var b = data.b || {
             minWidth: data.minWidth,
             maxWidth: data.maxWidth,
             minHeight: data.minHeight,
             maxHeight: data.maxHeight
         }
-        if(data._aspectRatio || e.shiftKey) {
-            var aspest = true;
-            var pMinWidth = b.minHeight * data.aspectRatio;
-            var pMinHeight = b.minWidth / data.aspectRatio;
-            var pMaxWidth = b.maxHeight * data.aspectRatio;
-            var pMaxHeight = b.maxWidth / data.aspectRatio;
+        if(data._aspectRatio || event.shiftKey) {
+            var aspest = true,
+                pMinWidth = b.minHeight * data.aspectRatio,
+                pMinHeight = b.minWidth / data.aspectRatio,
+                pMaxWidth = b.maxHeight * data.aspectRatio,
+                pMaxHeight = b.maxWidth / data.aspectRatio;
 
             if(pMinWidth > b.minWidth) {
                 b.minWidth = pMinWidth;
@@ -84,36 +83,36 @@ define("resizable", ["mass.draggable"], function($) {
         }
 
         if(data.dir.indexOf("e") != -1) {
-            var width = data.startWidth + e.pageX - data.startX;
+            var width = data.startWidth + event.pageX - data.startX;
             width = Math.min(Math.max(width, b.minWidth), b.maxWidth);
             data.width = width;
-            if(aspest){
+            if(aspest) {
                 data.height = width / data.aspectRatio;
             }
         }
         if(data.dir.indexOf("s") != -1) {
-            var height = data.startHeight + e.pageY - data.startY;
+            var height = data.startHeight + event.pageY - data.startY;
             height = Math.min(Math.max(height, b.minHeight), b.maxHeight);
             data.height = height;
-            if(aspest){
+            if(aspest) {
                 data.width = height * data.aspectRatio;
             }
         }
         if(data.dir.indexOf("w") != -1) {
-            data.width = data.startWidth - e.pageX + data.startX;
+            data.width = data.startWidth - event.pageX + data.startX;
             if(data.width >= b.minWidth && data.width <= b.maxWidth) {
-                data.left = data.startLeft + e.pageX - data.startX;
-                if(aspest){
-                    data.top =  data.startTop + (e.pageX - data.startX)  / data.aspectRatio;
+                data.left = data.startLeft + event.pageX - data.startX;
+                if(aspest) {
+                    data.top = data.startTop + (event.pageX - data.startX) / data.aspectRatio;
                 }
             }
         }
         if(data.dir.indexOf("n") != -1) {
-            data.height = data.startHeight - e.pageY + data.startY;
+            data.height = data.startHeight - event.pageY + data.startY;
             if(data.height >= b.minHeight && data.height <= b.maxHeight) {
-                data.top = data.startTop + e.pageY - data.startY;
-                if(aspest){
-                    data.left =  data.startLeft + (e.pageY - data.startY) *  data.aspectRatio;
+                data.top = data.startTop + event.pageY - data.startY;
+                if(aspest) {
+                    data.left = data.startLeft + (event.pageY - data.startY) * data.aspectRatio;
                 }
             }
         }
@@ -124,76 +123,76 @@ define("resizable", ["mass.draggable"], function($) {
             height: data.height
         });
     }
-
-    $.fn.resizable = function(hash) {
-        var config = $.mix({}, defaults, hash || {});
-        config.handles = config.handles.match($.rword) || ["all"];//
-        if(!/^(x|y|xy)$/.test(config.axis)) {
-            config.axis = ""; //如果用户没有指定,就禁止拖动
-        }
-        config._aspectRatio = !! config.aspectRatio
-        this.each(function() {
-            if(this.nodeType == 1) {
-                //当鼠标在拖动元素上移动时,在它们靠近边缘的那一霎修改光标样式
-                $(this).bind('mousemove.resizable', config.selector, function(e) {
-                    var target = $(this)
-                    if(facade.dragger) return;
-                    var dir = getDirection(e, target, config);
-                    $._data(this, "cursor", target.css("cursor"));//保存原来的光标样式
-                    if(dir == "") {
-                        target.css("cursor", "default");
-                    } else {
-                        target.css("cursor", dir + "-resize");
-                    }
-                }).bind("mouseleave.resizable", function(e) {
-                    $(this).css("cursor", $._data(this,"cursor")); //还原光标样式
-                })
+    var draggable = $.fn.draggable;
+    var resizable = $.fn.resizable = function(hash) {
+            var config = $.mix({}, defaults, hash || {});
+            config.handles = config.handles.match($.rword) || ["all"]; //
+            if(!/^(x|y|xy)$/.test(config.axis)) {
+                config.axis = ""; //如果用户没有指定,就禁止拖动
             }
+            config._aspectRatio = !! config.aspectRatio
+            this.each(function() {
+                if(this.nodeType == 1) {
+                    //当鼠标在拖动元素上移动时,在它们靠近边缘的那一霎修改光标样式
+                    $(this).bind('mousemove.resizable', config.selector, function(e) {
+                        var target = $(this)
+                        if(draggable.dragger) return;
+                        var dir = getDirection(e, target, config);
+                        $._data(this, "cursor", target.css("cursor")); //保存原来的光标样式
+                        if(dir == "") {
+                            target.css("cursor", "default");
+                        } else {
+                            target.css("cursor", dir + "-resize");
+                        }
+                    }).bind("mouseleave.resizable", config.selector, function(e) {
+                        $(this).css("cursor", $._data(this, "cursor")); //还原光标样式
+                    })
+                }
 
-        })
-        //在dragstart回调中,我们通过draggable已经设置了
-        //data.startX = event.pageX;    data.startY = event.pageY;
-        //data.originalX = offset.left; data.originalY = offset.top;
-        config.dragstart = function(e, data) {
-            var target = data.element;
-            var dir = getDirection(e, target, data);
-            if(dir == '') return;
-            $.mix(data, {
-                dir: dir,
-                startLeft: getCssValue(target, "left"),
-                startTop: getCssValue(target, "top"),
-                startWidth: target.width(),
-                startHeight: target.height()
-            });
-            "startLeft,startTop,startWidth,startHeight".replace($.rword, function(word) {
-                data[word.replace("start", "").toLowerCase()] = data[word];
-            });
-            //等比例缩放
-            data.aspectRatio = (typeof data.aspectRatio === "number") ? data.aspectRatio : ((data.startWidth / data.startHeight) || 1);
-            e.type = "resizestart";
-            data.resizestart.call(target[0], e, data); //触发用户回调
-            $('body').css('cursor', dir + '-resize');
-        }
-        config.drag = function(e, data) {
-            if(data.dir) {
+            })
+            //在dragstart回调中,我们通过draggable已经设置了
+            //data.startX = event.pageX;    data.startY = event.pageY;
+            //data.originalX = offset.left; data.originalY = offset.top;
+            config.dragstart = function(event, data) {
                 var target = data.element;
-                refresh(e, target, data);
-                e.type = "resize";
-                data.resize.call(target[0], e, data); //触发用户回调
+                var dir = getDirection(event, target, data);
+                if(dir == '') return;
+                $.mix(data, {
+                    dir: dir,
+                    startLeft: getCssValue(target, "left"),
+                    startTop: getCssValue(target, "top"),
+                    startWidth: target.width(),
+                    startHeight: target.height()
+                });
+                "startLeft,startTop,startWidth,startHeight".replace($.rword, function(word) {
+                    data[word.replace("start", "").toLowerCase()] = data[word];
+                });
+                //等比例缩放
+                data.aspectRatio = (typeof data.aspectRatio === "number") ? data.aspectRatio : ((data.startWidth / data.startHeight) || 1);
+                event.type = "resizestart";
+                data.resizestart.call(target[0], event, data); //触发用户回调
+                $('body').css('cursor', dir + '-resize');
             }
-        }
-        config.dragend = function(e, data) {
-            if(data.dir) {
-                var target = data.element;
-                refresh(e, target, data);
-                delete data.dir;
-                e.type = "resizeend";
-                data.resizeend.call(target[0], e, data); //触发用户回调
-                $('body').css("cursor", "default");
+            config.drag = function(event, data) {
+                if(data.dir) {
+                    var target = data.element;
+                    refresh(event, target, data);
+                    event.type = "resize";
+                    data.resize.call(target[0], event, data); //触发用户回调
+                }
             }
+            config.dragend = function(event, data) {
+                if(data.dir) {
+                    var target = data.element;
+                    refresh(event, target, data);
+                    delete data.dir;
+                    event.type = "resizeend";
+                    data.resizeend.call(target[0], event, data); //触发用户回调
+                    $('body').css("cursor", "default");
+                }
+            }
+            return this.draggable(config)
         }
-        return this.draggable(config)
-
-    }
+    resizable.defaults = defaults;
     return $;
 })
