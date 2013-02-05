526d428ad73b3e690bd6dfc7878a82c1b684d1f9
RubyLouvre <cheng19840218@gmail.com>
2013-02-05 23:55:07 +0800

    完成border布局

--- mass.layout.js
+++ mass.layout.js
@@ -28,16 +28,12 @@ define("mass.layout", ["css", "attr"], function($) {
             w.push(parseFloat(el.outerWidth(true)));
             h.push(parseFloat(el.outerHeight(true)));
         });
-
         var maxWidth = Math.max.apply(0, w);
         var maxHeight = Math.max.apply(0, h);
-
         var row = 0, col, delta, left, top;
         var cols = parseFloat(opts.cols) || children.length;
-
         var hgap = parseFloat(opts.hgap) || 1;
         var vgap = parseFloat(opts.vgap) || 1;
-
         for (var i = 0, el; el = children[i]; i++) {
 
             col = i % cols;
@@ -46,7 +42,6 @@ define("mass.layout", ["css", "attr"], function($) {
             }
             left = (maxWidth + hgap) * col;
             top = (maxHeight + vgap) * row;
-
             el.css({
                 top: top,
                 left: left
@@ -60,7 +55,7 @@ define("mass.layout", ["css", "attr"], function($) {
                 el.height("+=" + delta);
             }
         }
-        elem.width(1);//通过这种方式完美消息滚动条
+        elem.width(1); //通过这种方式完美消息滚动条
         elem.width(elem[0].scrollWidth);
         elem.height(1);
         elem.height(elem[0].scrollHeight);
@@ -75,7 +70,7 @@ define("mass.layout", ["css", "attr"], function($) {
             callback(el)
             var delta = max - array.values[i]
             if (delta) {
-                el[prop]("+=" + delta);//补高
+                el[prop]("+=" + delta); //补高
             }
         }
         obj[prop] += max;
@@ -94,16 +89,15 @@ define("mass.layout", ["css", "attr"], function($) {
         var cols = parseFloat(opts.cols) || children.length;
         var hgap = parseFloat(opts.hgap) || 1;
         var vgap = parseFloat(opts.vgap) || 1;
-
         var colsObject = {};
         var rowsObject = {};
         var outerObject = {
             width: 0,
             height: 0
-        }
+        };
         //处理每一行的高与top值
         for (var i = 0, el; el = children[i]; i++) {
-            var col = i % cols;//当前列数
+            var col = i % cols; //当前列数
             var colsArray = colsObject[col];
             if (!colsArray) {
                 colsArray = colsObject[col] = [];
@@ -117,7 +111,7 @@ define("mass.layout", ["css", "attr"], function($) {
                 rowsArray.values = [];
             }
             rowsArray.push(el);
-            rowsArray.values.push(h[i])
+            rowsArray.values.push(h[i]);
             //如果到每行的最后一个
             if (col === cols - 1) {
                 row++;
@@ -140,6 +134,86 @@ define("mass.layout", ["css", "attr"], function($) {
                 }, outerObject);
             }
         }
+        elem.width(1);
+        elem.width(elem[0].scrollWidth);
+        elem.height(1);
+        elem.height(elem[0].scrollHeight);
+    };
+
+    function adjustHeight(item, top, width) {
+        item.css({
+            top: top,
+            left: 0
+        });
+        var itemWidth = item.outerWidth(true);
+        var delta = width - itemWidth;
+        if (delta > 0) {
+            item.width("+=" + delta); //补高
+        } else if (delta < 0) {
+            item.width(width)
+        }
+    }
+//┌────────────┐
+//│　　 　　north　　　　 │
+//├──┬─────┬───┤
+//│west│ center  │east │
+//├──┴─────┴───┤
+//│　　 　　south　　　　 │
+//└────────────┘
+//border 布局：border 布局也称边界布局，他将页面分隔为 west,east,south,north,center 这五个部分， 我们需要在在其 items 中指定 
+//使用 region 参数为其子元素指定具体位置。 注意：north 和 south 部分只能设置高度（height），
+//west 和 east 部分只能设置宽度（width）。north south west east 区域变大，center 区域就变小了。
+    $.fn.layout.border = function(elem, opts) {
+        var items = {};
+        "north,south,west,east,center".replace($.rword, function(name) {
+            items[name] = elem.find("[data-region=" + name + "]:visible");
+            if (!items[name].length) {
+                delete items[name];
+            } else {
+                items[name][0].style.position = "absolute";
+            }
+        });
+        if (!items.center) {
+            throw "必须指定center区域并且让其可见"
+        }
+        elem[0].style.position = "relative";
+        var hgap = parseFloat(opts.hgap) || 1;
+        var vgap = parseFloat(opts.vgap) || 1;
+        var h = [], w = [], els = [], outerWidth = 0, outerHeight = 0;//收集中间格子的宽高与元素
+        "west,center,east".replace($.rword, function(name) {
+            if (items[name]) {
+                els.push(items[name]);
+                w.push(parseFloat(items[name].outerWidth(true)));
+                h.push(parseFloat(items[name].outerHeight(true)));
+            }
+        });
+        var maxHeight = Math.max.apply(0, h);
+        //重设中间格子的left值与高度
+        for (var i = 0, el; el = els[i]; i++) {
+            var delta = maxHeight - h[i];
+            el.css("left", outerWidth + hgap * i);
+            if (delta) {
+                el.height("+=" + delta); //补高
+            }
+            outerWidth += w[i];
+        }
+        outerWidth += (w.length - 1) * hgap;
+        //重设north区域的top值与宽度
+        var north = items.north;
+        if (north) {
+            adjustHeight(north, 0, outerWidth);
+            outerHeight = north.outerHeight(true) + vgap;
+        }
+        //重设south区域的top值与宽度
+        var south = items.south;
+        if (south) {
+            adjustHeight(south, outerHeight + maxHeight + vgap, outerWidth);
+        }
+        //重设中间区域的top值
+        els.forEach(function(el) {
+            el.css("top", outerHeight);
+        });
+        //=================================================
         elem.width(1);
         elem.width(elem[0].scrollWidth);
         elem.height(1);
