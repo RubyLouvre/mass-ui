f17f5a07b85670a572a244b8a71c9259ad9326d7
RubyLouvre <cheng19840218@gmail.com>
2013-03-13 20:46:17 +0800

    update!

--- mass.messenger.js
+++ mass.messenger.js
@@ -12,6 +12,10 @@ define(["node"], function($) {
         }
         this.win = win;
         this._messages = [];
+        var mode = document.documentMode;
+        if (mode === 8 || mode === 9) {
+            this.hack = true;
+        }
         //onmessage为当前页面处理其他页面发过来的
         if (typeof config.onmessage === "function") {
             this.receive(config.onmessage);
@@ -26,8 +30,8 @@ define(["node"], function($) {
                 if (event.source != me.win)
                     return;//如果不是来源自win所指向的窗口,返回
                 var data = event.data;
-                if (me._hack && data.indexOf(me.hack) === 0) {
-                    data = data.replace(me._hack, "");
+                if (typeof data === "string" && data.indexOf("__hack__") === 0) {
+                    data = data.replace("__hack__", "");
                     data = JSON.parse(data, function(k, v) {
                         if (v.indexOf && v.indexOf('function') > -1) {
                             return eval("(function(){return " + v + " })()")
@@ -39,10 +43,7 @@ define(["node"], function($) {
                     fn.call(me, data);
                 }
             };
-            var mode = document.documentMode;
-            if (mode === 8 || mode === 9) {
-                this._hack = String(new Date - 0);
-            }
+
             $.bind(window, "message", me._callback);
         },
         receive: function(fn) {
@@ -50,7 +51,18 @@ define(["node"], function($) {
             this._messages.push(fn);
         },
         send: function(data) {
-            if (this._hack && data && typeof data !== "string") {
+            var str = data, hack = false;
+            //如果对象中存在函数,会抛DataCloneError: DOM Exception 25异常,需要序列化
+            //http://stackoverflow.com/questions/7506635/uncaught-error-data-clone-err-dom-exception-25-thrown-by-web-worker
+            if (!this.hack && typeof data === "object") {
+                for (var i in data) {//这只是一个简单的检测,只检测一重
+                    if (data.hasOwnProperty(i) && typeof data[i] === "function") {
+                        hack = true;
+                        break;
+                    }
+                }
+            }
+            if (hack || (this.hack  && typeof str !== "string")) {
                 //在w3c规范中,data可以是任意数据类型,但IE8-9由于实现得够早,只支持字符串
                 //这时我们就需要用到JSON进行序列化与反序列化
                 //JavaScript primitive, such as a string
@@ -61,13 +73,13 @@ define(["node"], function($) {
                 //Blob
                 //File
                 //ArrayBuffer
-                data = JSON.stringify(data, function(key, val) {
+                str = JSON.stringify(str, function(key, val) {
                     if (typeof val === 'function') {
                         return val + '';
                     }
                     return val;
                 });
-                data = this._hack + data
+                data = "__hack__" + str;
             }
             this.win.postMessage(data, '*');//parent
         }
